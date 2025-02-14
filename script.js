7e2084dc83177af7e4b852418fa8138bb2fe537a
VulcanVar <alankritsaxena.weschool@gmail.com>
2025-02-15 04:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = ['Pichi','Picha','Peach', 'Peaches', 'Momo', 'Lichi', 'Pixhi', 'Oichi', 'Aishwarya', 'Momdi', 'Aisha', 'Miumiu', 'Mewmew', 'Kitty', 'Pichdi'; // Replace with your secret code
+//const secretCode = ['Pichi','Picha','Peach', 'Peaches', 'Momo', 'Lichi', 'Pixhi', 'Oichi', 'Aishwarya', 'Momdi', 'Aisha', 'Miumiu', 'Mewmew', 'Kitty', 'Pichdi'; // Replace with your secret code
+const secretCode = 'Pichi';
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
-	if (secretCodes.some(name => name.toLowerCase() === userInput.toLowerCase())) {
+	if (userInput === secretCode) {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
