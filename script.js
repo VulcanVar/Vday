318540327e886281ed9f078e053132bd2dc34388
VulcanVar <alankritsaxena.weschool@gmail.com>
2025-02-15 04:51:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Sona'; // Replace with your secret code
+const secretCode = ['Pichi','Picha','Peach', 'Peaches', 'Momo', 'Lichi', 'Pixhi', 'Oichi', 'Aishwarya', 'Momdi', 'Aisha', 'Miumiu', 'Mewmew', 'Kitty', 'Pichdi'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
-	if (userInput === secretCode) {
+	if (secretCodes.some(name => name.toLowerCase() === userInput.toLowerCase())) {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Sona';
+		resultDiv.innerHTML = 'You are not my Pichi, whu yu beech? >.> This is not for you! Go away!';
 	}
 });
